93f9ea474c2bbbb930d6b88ad89cc94988ba1abd
wilder <bertrandwcs>
2019-06-11 10:53:38 +0200

    reacteroute3

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import MyDreams from './components/MyDreams/index';
 import HomePage from './components/HomePage/index';
 import LogInForm from './components/LogInForm/index';
@@ -8,13 +8,12 @@ import LogInForm from './components/LogInForm/index';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
+      <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/mydreams/" component={MyDreams} />
           <Route path="/loginform/" component={LogInForm} />
-        </Switch>
-      </BrowserRouter>
+      </Switch>
+      
     </div>
   );
 }
