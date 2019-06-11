8af74004bacd931680c7cf005c2eefb3e87c9a35
Geoffrey Bascour <geoffreybascour93@gmail.comm>
2019-06-11 12:04:25 +0200

    link to back-end and front-end

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:3000")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
