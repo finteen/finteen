5387c69da655ab9ee4442936cf70272cd2066426
Alessandro Cavaleri <caval1289@gmail.com>
2019-06-17 11:32:44 +0200

    maj

--- back/app.js
+++ back/app.js
@@ -2,17 +2,10 @@ const express = require ('express');
 const app =  express();
 require('dotenv').config()
 const login = require('./routes/login/login')
-
-//const loginRouter = require("./routes/auth/login");
-//const sequilize =  require('./helpers/conf')
-
-
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({extend: false}));
 app.use(bodyParser.json());
-//app.use('/conf', sequilise);
-//app.use('/auth',loginRouter);
-
 app.use('/auth', login )
 
 const server = app.listen( process.env.PORT  ||  5000, function(){
