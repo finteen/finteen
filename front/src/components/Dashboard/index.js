4f15822ef25788fa9ceca0ba3328e4157efaf5d8
Geoffrey Bascour <geoffreybascour93@gmail.comm>
2019-06-25 10:30:25 +0200

    dashboard

--- front/src/components/Dashboard/index.js
+++ front/src/components/Dashboard/index.js
@@ -1,19 +1,19 @@
-
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './index.css';
 
 const Dashboard = () => (
   <div>
-    <div>
+    <div className="container-my-budget">
       <NavLink to="/mybudget" className="my-budget">My Budget</NavLink>
     </div>
-    <div>
+    <div className="container-my-dreams">
       <NavLink to="/mydreams" className="my-dreams">My Dreams</NavLink>
     </div>
-    <div>
+    <div className="container-my-rewards">
       <NavLink to="/myrewards" className="my-rewards">My Rewards</NavLink>
     </div>
-    <div>
+    <div className="container-my-profils">
       <NavLink to="/myprofils" className="my-profils">My Profils</NavLink>
     </div>
   </div>
