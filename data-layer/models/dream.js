9e64ad25aa0f490290f9964b38915174300f1b1e
rob17833 <janssensrobert@hotmail.com>
2019-06-27 15:01:08 +0200

    update dream model

--- data-layer/models/dream.js
+++ data-layer/models/dream.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: true
     },
     dream_description: {
-      type: Sequelize.STRING,
+      type: DataTypes.STRING,
       allowNull:false
     },
   }, {freezeTableName: true, timestamps: false});
