de25daa2806f2c38782d19a20ca23e286043ebe3
rob17833 <janssensrobert@hotmail.com>
2019-06-27 14:01:57 +0200

    change datatype

--- data-layer/seeders/20190621134208-generate-kid.js
+++ data-layer/seeders/20190621134208-generate-kid.js
@@ -1,39 +1,40 @@
 'use strict';
+const items = [
+  {
+    name: 'Laura'
+    lastname: 'Bonchard'
+    email: 'laurabonchard@gmail.com'
+    pseudonyme: 'laurala2006'
+    age:2006-06-12 
+    password: 'lauragere06'
+    iban: 'FR7630001007941234567890185'
+    createdAt: 
+    updatedAt: 
+  },
+  {
+    name: 'Ben'
+    lastname: 'Jünger Klopp'
+    email: 'benjungerklopp@gmail.com'
+    pseudonyme: 'benliverpool2019'
+    age:2006-06-12 
+    password: 'bensalah04'
+    iban: 'BE03310129669484'
+  }
+];
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    
-    return queryInterface.bulkInsert('kid', [
-      {
-        name: 'Laura'
-        lastname: 'Bonchard'
-        email: 'laurabonchard@gmail.com'
-        pseudonyme: 'laurala2006'
-        age:2006-06-12 
-        password: 'lauragere06'
-        iban: 'FR7630001007941234567890185'
-        createdAt: 
-        updatedAt: 
-      },
-      {
-        name: 'Ben'
-        lastname: 'Jünger Klopp'
-        email: 'benjungerklopp@gmail.com'
-        pseudonyme: 'benliverpool2019'
-        age:2006-06-12 
-        password: 'bensalah04'
-        iban: 'BE03310129669484'
-      }]);
-  
+    return queryInterface.bulkInsert('kid', items, {freezeTableName: true});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+     return queryInterface.bulkDelete(
+      'kid',
+      {
+        id: { [Sequelize.Op.in]: items.map(item => item.id) },
+      },
+      {freezeTableName: true}
+    );
   }
 };
+
