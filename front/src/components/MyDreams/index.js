e56f30f071c98f6aa0b0377272cbe58db6343628
Cardon <bertrandcardondel@gmail.com>
2019-07-01 16:24:47 +0200

    designdashboard

--- front/src/components/MyDreams/index.js
+++ front/src/components/MyDreams/index.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import './index.css';
+import {
+  NavLink, Row, Container, Col,
+}
+  from 'reactstrap';
+import LogoBudget from '../../Assets/images/logomybudget.jpg';
+import LogoDreams from '../../Assets/images/logomydreams.jpg';
+import LogoRewards from '../../Assets/images/logomyrewards.jpg';
+import LogoProfile from '../../Assets/images/logomyprofils.jpg';
 import BurgerMenu from '../BurgerMenu/index';
 
 
-const MyDreams = () => (
+const Mydreams = () => (
 
   <div>
     <BurgerMenu />
-    <h1>My Dreams</h1>
+    <Container>
+      <Row>
+        <Col xs="6">
+          <NavLink href="/mybudget" className="my-budget">
+            <img src={LogoBudget} alt="logomybudget" />
+          </NavLink>
+        </Col>
+        <Col xs="6">
+          <NavLink href="/mydreams" className="my-dreams">
+            <img src={LogoDreams} alt="logomydreams" />
+          </NavLink>
+        </Col>
+        <Col xs="6">
+          <NavLink href="/myrewards" className="my-rewards">
+            <img src={LogoRewards} alt="logomyrewards" />
+          </NavLink>
+        </Col>
+        <Col xs="6">
+          <NavLink href="/myprofile" className="my-profile">
+            <img src={LogoProfile} alt="myprofils" />
+          </NavLink>
+        </Col>
+      </Row>
+    </Container>
   </div>
 );
 
-export default MyDreams;
+export default Mydreams;
