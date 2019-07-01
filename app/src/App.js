97621e8fe10314bfffe5e93d423656d257e31325
Alessandro Cavaleri <caval1289@gmail.com>
2019-07-01 15:38:38 +0200

    changement route

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import HomePage from './components/HomePage/index';
 import LoginPage from './components/LoginPage/index';
 import Dashboard from './components/Dashboard/index';
 import SignupPage from './components/SignupPage/index';
@@ -11,8 +10,7 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/loginform/" component={LoginPage} />
+        <Route exact path="/" component={LoginPage} />
         <Route path="/dashboard/" component={Dashboard} />
         <Route path="/signupform/" component={SignupPage} />
       </Switch>
