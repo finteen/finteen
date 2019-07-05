acefd59db5d8c571c0e979b9742215d3166c4566
Geoffrey Bascour <geoffreybascour93@gmail.comm>
2019-07-05 14:47:51 +0200

    "no conflits

--- app/src/components/MyDreams/index.js
+++ app/src/components/MyDreams/index.js
@@ -8,20 +8,6 @@ const MyDreams = () => (
   <div>
     <BurgerMenu />
     <h1>My Dreams</h1>
-    <div className="card">
-      <img src="img_avatar.png" alt="Avatar" />
-      <div className="container">
-        <h2><b>Playsatation 4</b></h2>
-        <p>Prix : 251€</p>
-      </div>
-    </div>
-    <div className="card">
-      <img src="img_avatar.png" alt="Avatar" />
-      <div className="container">
-        <h4><b>Sac Michael Kors</b></h4>
-        <p>Prix </p>
-      </div>
-    </div>
   </div>
 );
 
