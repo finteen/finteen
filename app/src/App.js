c4e136520eccc91de9cda8e87f6da759be456aac
Geoffrey Bascour <geoffreybascour93@gmail.comm>
2019-07-09 09:27:46 +0200

    my dreams

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,7 @@ import HomePage from './components/HomePage/index';
 import LoginPage from './components/LoginPage/index';
 import Dashboard from './components/Dashboard/index';
 import SignupPage from './components/SignupPage/index';
-
+import MyDreamsPage from './components/MyDreamsPage/index';
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Route exact path="/" component={LoginPage} />
         <Route path="/dashboard/" component={Dashboard} />
         <Route path="/signupform/" component={SignupPage} />
+        <Route path="/mydreams/" component={MyDreamsPage} />
       </Switch>
     </div>
   );
