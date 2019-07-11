be80c2d82a668c3831dece7272719d91413c6251
Cardon <bertrandcardondel@gmail.com>
2019-07-11 09:58:01 +0200

    my profile front en cours

--- app/src/components/MyProfilePage/index.js
+++ app/src/components/MyProfilePage/index.js
@@ -2,13 +2,36 @@ import React from 'react';
 import './index.css';
 import BurgerMenu from '../BurgerMenu/index';
 
+const url = 'http://localhost:3000/auth/profile';
 
-const MyProfilePage = () => (
+class MyProfilePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {},
+    };
+  }
 
-  <div>
-    <BurgerMenu />
-    <h1>My Profile</h1>
-  </div>
-);
+  componentDidMount() {
+    fetch(url)
+      .then(res => res.json());
+      .then((res) => {
+        if (res.status === 200) {
+          this.setState({ data: res });
+        }
+      })
+  }
 
+
+  render() {
+    const { data } = this.state;
+    console.log(data)
+    return (
+      <div>
+        <BurgerMenu />
+        <h1>{data.name}</h1>
+      </div>
+    );
+  }
+}
 export default MyProfilePage;
