1330444e8a03b016639e43d28069949ddd06be0d
Alessandro Cavaleri <caval1289@gmail.com>
2019-07-11 10:58:21 +0200

    pie chart

--- app/src/components/MyBudgetPage/index.js
+++ app/src/components/MyBudgetPage/index.js
@@ -1,30 +1,16 @@
 import React from 'react';
-<<<<<<< HEAD
-import DataBudget from '../DataBudget/index';
-
-
-class MyBudget extends React.Component {
-    render() {
-        return (
-            <div>
-                <DataBudget />
-            </div>
-        )
-    }
-}
-export default MyBudget;
-=======
 import './index.css';
 import BurgerMenu from '../BurgerMenu/index';
+import Graph from '../Graph/index';
 
 
-const MyBudgetPage = () => (
-
-  <div>
-    <BurgerMenu />
-    <h1>My Budget</h1>
-  </div>
-);
+function MyBudgetPage() {
+  return (
+    <div>
+      <BurgerMenu />
+      <Graph />
+    </div>
+  );
+}
 
 export default MyBudgetPage;
->>>>>>> 8b6ab496031ba3f05f62fd79426847885bf876b5
