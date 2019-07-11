5fe49bd29881fa76e0e8e6a4283c45cb80ef0644
rob17833 <janssensrobert@hotmail.com>
2019-07-11 12:28:21 +0200

    chart from data done

--- app/src/components/MyBudgetPage/index.js
+++ app/src/components/MyBudgetPage/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './index.css';
 import BurgerMenu from '../BurgerMenu/index';
+import Graph from '../Graph/index';
 
-
-const MyBudgetPage = () => (
-
-  <div>
-    <BurgerMenu />
-    <h1>My Budget</h1>
-  </div>
-);
-
+function MyBudgetPage() {
+  return (
+    <div>
+      <BurgerMenu />
+      <Graph />
+    </div>
+  );
+}
 export default MyBudgetPage;
