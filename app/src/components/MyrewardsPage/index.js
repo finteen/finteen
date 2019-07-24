4377c6ac175c9342663c1f4d9eaa5a362ed087fe
Cardon <bertrandcardondel@gmail.com>
2019-07-24 11:02:43 +0200

    debug general

--- app/src/components/MyrewardsPage/index.js
+++ app/src/components/MyrewardsPage/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './index.css';
 import BurgerMenu from '../BurgerMenu/index';
-
+import Rewards from '../../Assets/images/unnamed.jpg';
 
 const MyrewardsPage = () => (
-
   <div>
     <BurgerMenu />
-    <h1>My Rewards</h1>
+    <div className="n">
+      <h2>My Rewards</h2>
+      <img src={Rewards} alt="rewards" className="rewards" />
+    </div>
   </div>
 );
-
 export default MyrewardsPage;
